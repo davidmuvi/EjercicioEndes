f42b923da826019f0c38d9120a65beed21646506
Iván García <ivancitogc22@gmail.com>
2023-02-23 18:45:24 +0100

    Función suma actualizada

--- suma.py
+++ suma.py
@@ -1,5 +1,11 @@
 # Función que suma dos números
 def sumar(num1, num2):
+    """Sumar dos números.
+
+    :param num1:
+    :param num2:
+    :return:
+    """
     suma = num1 + num2
 
     return suma
